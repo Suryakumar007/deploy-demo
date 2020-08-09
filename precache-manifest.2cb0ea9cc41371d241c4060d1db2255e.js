239741bec5dffc8f64a77cb018ef5062cb0ad15e
Suryakumar Arumugam <asdsurya@gmail.com>
2020-08-09 15:50:13 +0530

    Updates

---
+++ precache-manifest.2cb0ea9cc41371d241c4060d1db2255e.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "4ab3b41f89d45bc817b567524ef0ee48",
+    "url": "/deploy-demo/index.html"
+  },
+  {
+    "revision": "4de02ea7b1720b4b5996",
+    "url": "/deploy-demo/static/css/main.5f361e03.chunk.css"
+  },
+  {
+    "revision": "733cb5d56f78c84ed33b",
+    "url": "/deploy-demo/static/js/2.2c5795d0.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/deploy-demo/static/js/2.2c5795d0.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "4de02ea7b1720b4b5996",
+    "url": "/deploy-demo/static/js/main.b430e41e.chunk.js"
+  },
+  {
+    "revision": "d6ff1a59a47fd2599cef",
+    "url": "/deploy-demo/static/js/runtime-main.f0d6fad3.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/deploy-demo/static/media/logo.5d5d9eef.svg"
+  }
+]);
